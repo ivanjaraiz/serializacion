7e4af7f370b3bb7057b465bbf14495224d502f8c
IJM <ivan.jaraiz@gmail.com>
2020-05-24 19:01:36 +0200

    se genera un metodo de lectura de datos desde fichero al programa y saco por pantalla para validar.

--- serializacion.cpp
+++ serializacion.cpp
@@ -12,25 +12,41 @@ class Student
         string branch;
         friend ofstream & operator<<(ofstream & ofs, Student & s); // fichero y objeto. Inserta operador en el file, 
         // no en la consola. 
+        friend ifstream& operator>>(ifstream& ifs, Student& s);
 };
 
 ofstream & operator<<(ofstream & ofs, Student & s) // Tengo sobrecargado el operador. 
-{
+{// SALIDA DE DATOS DESDE EL PROGRAMA A FICHERO
     ofs << s.name << endl;
     ofs << s.roll << endl;
     ofs << s.branch << endl;
     return ofs;
 }
+
+ifstream& operator>>(ifstream& ifs, Student& s)// ENTRADA DE DATOS AL PROGRAMA DESDE FICHERO
+{
+    ifs >> s.name >> s.roll >> s.branch;
+    return ifs;
+}
+
 int main()
 {
     Student s1;
-    s1.name = "Jose"; s1.roll = 20; s1.branch = "CS";
+    /*s1.name = "Jose"; s1.roll = 20; s1.branch = "CS";
     //Inicializar el objeto en el fichero. 
     ofstream ofs("Student.txt", ios::trunc); // si no hay nada allí lo trunco. 
     //ofs << s1.name << s1.roll << s1.branch << endl; // Esto no es necesario con la funció operator, directamente se 
     // envía el objeto como la siguiente instrucción. 
     ofs << s1;
-    ofs.close();
+    ofs.close();*/
+    ifstream ifs("Student.txt"); // no truncar, entonces sin argumento. 
+    ifs >> s1;
+
+    cout << "Name " << s1.name << endl;
+    cout << "Roll " << s1.roll << endl;
+    cout << "Branch " << s1.branch << endl;
+    ifs.close();
+
     return 0;
 }
 
